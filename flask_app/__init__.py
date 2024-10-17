1036bcb06b4d13d110bfa81705e8cad18e745dc5
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2024-10-17 11:48:51 -0700

    installed flask package and added flask app folders

---
+++ flask_app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os
+from dotenv import load_dotenv
+import logging
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("FLASK_SECRET_KEY")  
+
+# logging.basicConfig(filename='record.log', level=logging.ERROR)
+
+UPLOAD_FOLDER = '/static/images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+# MAX_CONTENT_LENGTH = 1 * 1024 * 1024
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_LENGTH
